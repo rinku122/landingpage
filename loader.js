02d73db02fc975c697bb3f34b0cc0e0c5f2c808a
devrinku <devrinku1290@gmail.com>
2020-06-06 17:21:40 +0530

    deploy

---
+++ loader.js
@@ -0,0 +1,7 @@
+const loader = document.querySelector(".loader");
+const loadercover = document.querySelector(".loaderholder");
+
+window.addEventListener("load", see);
+function see() {
+  loadercover.style.display = "none";
+}
